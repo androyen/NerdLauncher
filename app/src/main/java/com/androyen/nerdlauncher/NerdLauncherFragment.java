be11bb984a0902b137df620cf9f828344f4c6e2d
Androyen <androyen@aol.com>
2014-11-11 18:25:56 -0600

    Implement NerdLauncherFragment to check total apps

---
+++ app/src/main/java/com/androyen/nerdlauncher/NerdLauncherFragment.java
@@ -0,0 +1,32 @@
+package com.androyen.nerdlauncher;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.util.Log;
+
+import java.util.List;
+
+/**
+ * Created by rnguyen on 11/11/14.
+ */
+public class NerdLauncherFragment extends ListFragment {
+
+    private static final String TAG = NerdLauncherFragment.class.getSimpleName();
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent startupIntent = new Intent(Intent.ACTION_MAIN);
+        startupIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        PackageManager pm = getActivity().getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(startupIntent, 0);
+
+        Log.i(TAG, "I've found " + activities.size() + " activities.");
+    }
+}
